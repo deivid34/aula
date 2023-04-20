879905cbe8f90d1fdb72506218dd10d9d0cc84ba
deivid34 <deividmelega33@gmail.com>
2023-04-20 08:59:22 -0300

    first commit

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,14 @@
 const {Router} = require('express')
 
-const{createCart} = require('./controllers')
+const{createCart,getlistofcart, createusercart} = require('./controllers')
 
 const routes = Router()
 
 routes.post('/cart', createCart) 
+
+routes.get('/cart', getlistofcart) 
+
+routes.post('/cart/user-cart', createusercart) 
 
     module.exports = routes
     
